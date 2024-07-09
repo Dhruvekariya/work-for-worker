47f051a75e8ca3a97553557437086777e16813a8
Dhruv <90555164+Dhruvekariya@users.noreply.github.com>
2024-07-09 10:44:18 +0530

    Update DB URL

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 // connecting to database
 const connectDB = async () => {
-    const connectionUrl = process.env.DB_URI;
+    const connectionUrl = "mongodb+srv://lajvantikawale:215ujShSTLhBRzgj@cluster0.9fxzk0p.mongodb.net/"
     mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`Database connected successfully`))
         .catch((err) => console.log("Getting Error from DB connection" + err.message))
